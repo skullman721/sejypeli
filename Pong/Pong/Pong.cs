f04768880eb4936eff92ec6fab19912c8f58e782
skullman721 <skullman.sejypeli@gmail.com>
2014-02-06 08:12:30 -0800

    SWAG JOONA SAIT VIESTIN YOLO

---
+++ Pong/Pong/Pong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class Pong : PhysicsGame
+{
+    PhysicsObject pallo;
+    public override void Begin()
+    {
+        LuodaKentta();
+        // TODO: Kirjoita ohjelmakoodisi tähän
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+    void LuodaKentta()
+    {
+        pallo = new PhysicsObject(40.0, 40.0);              
+            Add(pallo);
+        pallo.Shape = Shape.Circle;
+        pallo.X = -200.0;
+        pallo.Y = 0.0;
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        Level.CreateBorders(1.0, false);
+        Level.Background.Color = Color.Aqua;
+        Camera.ZoomToLevel();
+        
+    }
+}
