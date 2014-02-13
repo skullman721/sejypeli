e2877f23002fcf33f5da73027c1e53d5fb2dd451
skullman721 <skullman.sejypeli@gmail.com>
2014-02-13 08:28:19 -0800

    lol

--- Pong/Pong/Pong.cs
+++ Pong/Pong/Pong.cs
@@ -12,24 +12,53 @@ public class Pong : PhysicsGame
     public override void Begin()
     {
         LuodaKentta();
+        AloitaPeli();
         // TODO: Kirjoita ohjelmakoodisi tähän
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        
     }
+    void LuoMaila(double x, double y)
+    {
+      PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        maila.Shape = Shape.Rectangle;
+        maila.X = x;
+        maila.Y = y;
+        maila.Restitution = 1.0;
+        Add(maila);
+        maila.Color = Color.Aquamarine;  
+        
+        void AsetaOhjaimet ()
+        {
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        }
+
+    }
+
+    void AloitaPeli()
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+    }
+
     void LuodaKentta()
     {
         pallo = new PhysicsObject(40.0, 40.0);              
-            Add(pallo);
+
+        Add(pallo);
         pallo.Shape = Shape.Circle;
+        pallo.Color = Color.Black;
         pallo.X = -200.0;
         pallo.Y = 0.0;
-        Vector impulssi = new Vector(500.0, 0.0);
-        pallo.Hit(impulssi);
 
-        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        LuoMaila(Level.Left + 20.0, 0.0);
+        LuoMaila(Level.Right - 20.0, 0.0);
+        
+
+
         Level.CreateBorders(1.0, false);
-        Level.Background.Color = Color.Aqua;
-        Camera.ZoomToLevel();
+        Level.Background.Color = Color.DarkRed;
+        Camera.ZoomToLevel ();
         
     }
 }
+
